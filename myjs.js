576fb74940b833042840e4ca810798a661385d9a
truan86 <truan@ukr.net>
2016-01-19 15:48:55 +0200

    new commit

---
+++ myjs.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var formApp = angular.module('formApp', []);
+
+formApp.controller('FormCntr', function ($scope) {
+    $scope.loginPattern = /^[a-z0-9_-]{3,15}$/;
+    $scope.emailPattern = /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/;
+    $scope.passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.{8,})/;
+    $scope.tellPattern = /^\d{3}-\d{2}-\d{2}$/;
+});
